3677d57a91623115896ed484384d526d00992ca2
Pacelle Henrique <pacellehenrique18@gmail.com>
2025-11-17 17:00:51 -0300

    Uptade Lox.java, Interpreter.java
    Na classe Interpreter criação dos métodos stringfy e interpret. Na classe Lox ajustes para utilização desses métodos e para RuntimeError.

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -10,8 +10,9 @@ import java.util.List;
 //import java.util.Scanner;
 
 public class Lox {
-
+  private static final Interpreter interpreter = new Interpreter();
   static boolean hadError = false;
+  static boolean hadRuntimeError = false;
 
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
@@ -30,6 +31,7 @@ public class Lox {
 
     // Indicate an error in the exit code.
     if (hadError) System.exit(65);
+    if (hadRuntimeError) System.exit(70);
   }
 
   private static void runPrompt() throws IOException {
@@ -54,11 +56,17 @@ public class Lox {
     // Stop if there was a syntax error.
     if (hadError) return;
 
-    System.out.println(new AstPrinter().print(expression));
+    interpreter.interpret(expression);
   }
 
   static void error(int line, String message) {
     report(line, "", message);
+  }
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +
+        "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
   }
 
   private static void report(int line, String where,
