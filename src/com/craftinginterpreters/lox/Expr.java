efbc9e3338c8303686e359b1dcee8dbce7a1018a
Pacelle Henrique <pacellehenrique18@gmail.com>
2025-12-03 23:00:44 -0300

    Create Stmt e update Expr
    Usando AST para ajustar classes.

--- src/com/craftinginterpreters/lox/Expr.java
+++ src/com/craftinginterpreters/lox/Expr.java
@@ -3,76 +3,81 @@ package com.craftinginterpreters.lox;
 import java.util.List;
 
 abstract class Expr {
-  // Visitor interface genérica
   interface Visitor<R> {
+    R visitAssignExpr(Assign expr);
     R visitBinaryExpr(Binary expr);
     R visitGroupingExpr(Grouping expr);
     R visitLiteralExpr(Literal expr);
     R visitUnaryExpr(Unary expr);
   }
-
-  // Cada Expr aceita um Visitor
-  abstract <R> R accept(Visitor<R> visitor);
-
-    // Expr.Binary
-    static class Binary extends Expr {
-      final Expr left;
-      final Token operator;
-      final Expr right;
-
-      Binary(Expr left, Token operator, Expr right) {
-        this.left = left;
-        this.operator = operator;
-        this.right = right;
-      }
-
-      @Override
-      <R> R accept(Visitor<R> visitor) {
-        return visitor.visitBinaryExpr(this);
-      }
+  static class Assign extends Expr {
+    Assign(Token name, Expr value) {
+      this.name = name;
+      this.value = value;
     }
 
-    // Expr.Grouping
-    static class Grouping extends Expr {
-      final Expr expression;
-
-      Grouping(Expr expression) {
-        this.expression = expression;
-      }
-
-      @Override
-      <R> R accept(Visitor<R> visitor) {
-        return visitor.visitGroupingExpr(this);
-      }
-    }
-
-    // Expr.Literal
-    static class Literal extends Expr {
-      final Object value;
-
-      Literal(Object value) {
-        this.value = value;
-      }
-
-      @Override
-      <R> R accept(Visitor<R> visitor) {
-        return visitor.visitLiteralExpr(this);
-      }
-    }
-
-    // Expr.Unary
-    static class Unary extends Expr {
-      final Token operator;
-      final Expr right;
-
-      Unary(Token operator, Expr right) {
-        this.operator = operator;
-        this.right = right;
-      }
-
-      @Override
-      <R> R accept(Visitor<R> visitor) {
-        return visitor.visitUnaryExpr(this);
-      }
+    final Token name;
+    final Expr value;
   }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitAssignExpr(this);
+    }
+  static class Binary extends Expr {
+    Binary(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
+    }
+
+    final Expr left;
+    final Token operator;
+    final Expr right;
+  }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBinaryExpr(this);
+    }
+  static class Grouping extends Expr {
+    Grouping(Expr expression) {
+      this.expression = expression;
+    }
+
+    final Expr expression;
+  }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitGroupingExpr(this);
+    }
+  static class Literal extends Expr {
+    Literal(Object value) {
+      this.value = value;
+    }
+
+    final Object value;
+  }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLiteralExpr(this);
+    }
+  static class Unary extends Expr {
+    Unary(Token operator, Expr right) {
+      this.operator = operator;
+      this.right = right;
+    }
+
+    final Token operator;
+    final Expr right;
+  }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitUnaryExpr(this);
+    }
+
+  abstract <R> R accept(Visitor<R> visitor);
 }
---
+++ src/com/craftinginterpreters/lox/Stmt.java
@@ -0,0 +1,49 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+
+abstract class Stmt {
+  interface Visitor<R> {
+    R visitBlockStmt(Block stmt);
+    R visitExpressionStmt(Expression stmt);
+    R visitPrintStmt(Print stmt);
+  }
+  static class Block extends Stmt {
+    Block(List<Stmt> statements) {
+      this.statements = statements;
+    }
+
+    final List<Stmt> statements;
+  }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBlockStmt(this);
+    }
+  static class Expression extends Stmt {
+    Expression(Expr expression) {
+      this.expression = expression;
+    }
+
+    final Expr expression;
+  }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitExpressionStmt(this);
+    }
+  static class Print extends Stmt {
+    Print(Expr expression) {
+      this.expression = expression;
+    }
+
+    final Expr expression;
+  }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitPrintStmt(this);
+    }
+
+  abstract <R> R accept(Visitor<R> visitor);
+}
