7955d781eb5982de3a348889fa52e000c1fb016c
Pacelle Henrique <pacellehenrique18@gmail.com>
2025-12-13 14:00:50 -0300

    Update Interpreter.java

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -228,6 +228,10 @@ class Interpreter implements Expr.Visitor<Object>,
             arguments.add(evaluate(argument));
         }
 
+        if (!(callee instanceof LoxCallable)) {
+            throw new RuntimeError(expr.paren,"Can only call functions and classes.");
+        }
+
         LoxCallable function = (LoxCallable)callee;
         return function.call(this, arguments);
     }
