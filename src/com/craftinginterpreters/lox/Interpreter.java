dce6734a64552aebd2882db6c69ec13069b55503
Pacelle Henrique <pacellehenrique18@gmail.com>
2025-12-14 10:05:13 -0300

    Update classes
    Atualização para interpretação de chamadas.

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -5,8 +5,25 @@ import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>,
                              Stmt.Visitor<Void> {
-    private Environment environment = new Environment();
-    
+    final Environment globals = new Environment();
+    private Environment environment = globals;
+
+    Interpreter() {
+        globals.define("clock", new LoxCallable() {
+            @Override
+            public int arity() { return 0; }
+
+            @Override
+            public Object call(Interpreter interpreter,
+                                List<Object> arguments) {
+                return (double)System.currentTimeMillis() / 1000.0;
+            }
+
+            @Override
+            public String toString() { return "<native fn>"; }
+        });
+    }
+        
     void interpret(List<Stmt> statements) {
         try {
             for (Stmt statement : statements) {
@@ -233,6 +250,11 @@ class Interpreter implements Expr.Visitor<Object>,
         }
 
         LoxCallable function = (LoxCallable)callee;
+        if (arguments.size() != function.arity()) {
+            throw new RuntimeError(expr.paren, "Expected " +
+                function.arity() + " arguments but got " +
+                arguments.size() + ".");
+        }
         return function.call(this, arguments);
     }
 
